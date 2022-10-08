613fa6ada2c14219b8793f00da66b842d821f366
fouziabenadida <benadiafouzia@gmail.com>
2022-10-08 21:31:17 +0300

    app bar

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-app>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon />
+      <v-icon class="mx-4" large> mdi-video </v-icon>
+      <v-toolbar-title>
+        <span class="title">VideoVue</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-text-field
+        solo
+        placeholder="Search..."
+        single-line
+        append-icon="mdi-magnify"
+        color="white"
+        class="mt-6"
+      ></v-text-field>
+      <v-spacer />
+      <v-btn class="ml-0" icon>
+        <v-icon> mdi-apps</v-icon>
+      </v-btn>
+      <v-btn class="ma-2" icon>
+        <v-icon> mdi-bell</v-icon>
+      </v-btn>
+      <div>
+        <v-menu
+          v-model="menu"
+          :close-on-content-click="false"
+          :nudge-witdh="250"
+          offset-x
+        >
+          <template v-slot:activator="{ on }">
+            <v-avatar size="40">
+              <img
+                v-on="on"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+              />
+            </v-avatar>
+          </template>
+          <v-card>
+            <v-list>
+              <v-list-item>
+                <v-list-item-avatar></v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title> Fouzia BENADIDA </v-list-item-title>
+                  <v-list-item-substile> VideoVue </v-list-item-substile>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-divider />
+            <v-list>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-icon>mdi-cog</v-icon>
+                </v-list-item-action>
+                <v-list-item-title> Settings </v-list-item-title>
+              </v-list-item>
+            </v-list>
+            <v-list>
+              <v-list-item>
+                <v-list-item-action>
+                  <v-switch v-model="theme" color="purple"></v-switch>
+                </v-list-item-action>
+                <v-list-item-title> Dark theme </v-list-item-title>
+              </v-list-item>
+            </v-list>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="primary" text @click="menu = false">
+                Çikiş Yap
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+      </div>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    menu: false,
+    theme: false,
+  }),
+};
+</script>
